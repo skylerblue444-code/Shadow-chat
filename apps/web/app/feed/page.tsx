051dbe41ce24286adb2cf723e886221b9af2200e
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:36:51 -0600

    Update page.tsx

--- apps/web/app/feed/page.tsx
+++ apps/web/app/feed/page.tsx
@@ -1,26 +1,13 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { getFeed } from "@/lib/posts";
-import { Post } from "@/types/post";
-import { PostCard } from "@/components/PostCard";
-import { PostComposer } from "@/components/PostComposer";
+import MotionWrapper from "../../components/MotionWrapper";
+import Post from "../../components/Post";
+import { mockPosts } from "../../lib/mockData";
 
 export default function FeedPage() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  function load() {
-    getFeed().then(setPosts);
-  }
-
-  useEffect(load, []);
-
   return (
-    <main className="p-6 space-y-4 max-w-xl mx-auto">
-      <PostComposer onPost={load} />
-      {posts.map((p) => (
-        <PostCard key={p.id} post={p} />
-      ))}
-    </main>
+    <MotionWrapper>
+      <div className="max-w-xl mx-auto space-y-4">
+        {mockPosts.map(p => <Post key={p.id} post={p} />)}
+      </div>
+    </MotionWrapper>
   );
 }
