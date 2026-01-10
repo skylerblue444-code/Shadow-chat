e37d9774535e5f263da2c426f5f2348fc085833f
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:04:02 -0600

    Update page.tsx

--- apps/web/app/messages/page.tsx
+++ apps/web/app/messages/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { ChatRoom } from "@/components/ChatRoom";
+import ChatRoom from "../../components/ChatRoom";
 
 export default function MessagesPage() {
-  return (
-    <main className="p-6 max-w-xl mx-auto">
-      <h1 className="text-lg mb-4">Messages</h1>
-      <ChatRoom chatId="global" />
-    </main>
-  );
+  return <ChatRoom />;
 }
