d39b95ce065c739cb94a64689fd293104ef87727
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:11:29 -0600

    Create WalletSidebar.tsx

---
+++ apps/web/components/WalletSidebar.tsx
@@ -0,0 +1,9 @@
+import WalletConnect from "./WalletConnect";
+
+export default function WalletSidebar() {
+  return (
+    <aside className="bg-panel p-4 rounded">
+      <WalletConnect />
+    </aside>
+  );
+}
