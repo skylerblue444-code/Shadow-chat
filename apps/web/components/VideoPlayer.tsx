098cbff7adcc79534b0e18fac19796130e00ceb6
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:14:31 -0600

    Create VideoPlayer.tsx

---
+++ apps/web/components/VideoPlayer.tsx
@@ -0,0 +1,7 @@
+export default function VideoPlayer() {
+  return (
+    <div className="bg-black rounded h-64 flex items-center justify-center">
+      <span className="text-gray-400">Live Video Stream</span>
+    </div>
+  );
+}
