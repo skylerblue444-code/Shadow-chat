943f36f991e464926c445a8ea2bac5c7ff94e581
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:26:20 -0600

    Create VRScene.tsx

---
+++ apps/web/components/VRScene.tsx
@@ -0,0 +1,7 @@
+export default function VRScene() {
+  return (
+    <div className="bg-black h-80 rounded flex items-center justify-center">
+      <span className="text-gray-400">WebXR / Three.js Scene Placeholder</span>
+    </div>
+  );
+}
