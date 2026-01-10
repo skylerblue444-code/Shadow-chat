15e3232ea5dd216394c6cffd60e0e9ae358163c2
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 13:23:27 -0600

    Create page.tsx

---
+++ apps/web/app/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-lg">Settings</h1>
+      <p>Preferences coming soon</p>
+    </main>
+  );
+}
