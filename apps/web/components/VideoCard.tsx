41bf4d4926618a0c5091aad9d09fb8e3c0a84f9e
skylerblue444-code <skylerblue444@icloud.com>
2026-01-10 14:12:52 -0600

    Create VideoCard.tsx

---
+++ apps/web/components/VideoCard.tsx
@@ -0,0 +1,8 @@
+export default function VideoCard() {
+  return (
+    <div className="bg-panel p-4 rounded">
+      <div className="h-32 bg-bg mb-2" />
+      <h4>Live Stream</h4>
+    </div>
+  );
+}
