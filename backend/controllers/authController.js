be198f3fb2208fa2991036bafe16fdf04cc4e7f9
skylerblue444-code <skylerblue444@icloud.com>
2026-03-08 00:48:56 -0600

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+exports.registerUser = (req, res) => {
+  res.json({ message: "Register user placeholder" });
+};
+
+exports.loginUser = (req, res) => {
+  res.json({ message: "Login user placeholder" });
+};
